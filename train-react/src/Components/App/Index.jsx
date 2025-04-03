11e107c0282d1d56b3a72e30a1ab7d76e510848a
Kevinmineir <kevinmineiro2007@gmail.com>
2025-04-03 17:56:06 -0300

    Uso Do Metodo Map() Para Melhores Componentes

--- train-react/src/Components/App/Index.jsx
+++ train-react/src/Components/App/Index.jsx
@@ -2,6 +2,7 @@ import {react} from 'react';
 
 import { Travelheader } from '../header/index';
 import { Travel } from "../Location/index";
+
 import { travelData } from '../Location/data';
 
 export function TravelJournal(){
--- train-react/src/main.jsx
+++ train-react/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { react } from 'react';
 
 
-import { TravelJournal } from './Components/App';
+import { TravelJournal } from './Components/App/Index';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
