27a0bc505d5eb0e5d928f9d0eb8d07517111709d
Kevinmineir <kevinmineiro2007@gmail.com>
2025-04-29 19:51:40 -0300

    Adicionamento de Conditional Rendering

--- train-react/src/Components/Main/index.jsx
+++ train-react/src/Components/Main/index.jsx
@@ -8,42 +8,79 @@ export function Main() {
     const IngredientsList = Ingredients.map((ingredient) => {
         return (
             <>
-            <ul className='Lista-content'>
-            <li key={ingredient} className='List-item'>{ingredient}</li>
-            </ul> 
+            <li key={ingredient} className='List-item'>{ingredient}</li>        
             </>
         )
     })
 
-    function SubmitIngredient(event){
-        event.preventDefault();
 
-        const formdata = new FormData(event.currentTarget)
-        const newIngredient = formdata.get("Ingredient")
-        
-        SetIngredients(PrevIngredients => [...PrevIngredients, newIngredient])
 
-        event.currentTarget.reset()
-    } 
+    function RenderList(){
+       if (Ingredients.length >= 4) {
+        return (
+        <div>
+            <div className='Lista-Container'>
+                <span className='Lista-text'>Lista De Ingredientes :</span>
+            </div>
+            
+        <ul className='Lista-content'>
+        {IngredientsList}
+        </ul>
+
+        <button className='Clear-Button' onClick={ClearIngredient}>Limpar Ingredientes</button>
+
+        <div className='RecipeGenerator-Container'>
+                <div className='RecipeGeneratorText-Container'>
+                <span className='RecipeGeneratorText'>Pronto Para a Receita?</span>
+                <span className='RecipeGeneratorDesc'>Criar Uma Receita De Acordo Com Sua Lista De Ingredientes</span>
+                </div>
+                <div className='RecipeGenerator-Button-container'>
+                <button className='RecipeGenerator-Button'>Criar Receita</button>
+                </div>
+            </div>
+        </div>
+        )
+    }
+        if (Ingredients.length > 0 && Ingredients.length < 4) {
+            return (
+            <div>
+                <div className='Lista-Container'>
+                    <span className='Lista-text'>Lista De Ingredientes :</span>
+                </div>
+                
+            <ul className='Lista-content'>
+            {IngredientsList}
+            </ul>
+    
+            <button className='Clear-Button' onClick={ClearIngredient}>Limpar Ingredientes</button>
+          </div>
+        )    
+    }  
+    }
    
+    function ClearIngredient(){
+        SetIngredients([])
+    }
+
+    function AddIngredient(FormData){
+        const newIngredient = FormData.get("Ingredient")
+        SetIngredients(PrevIngredients => [...PrevIngredients, newIngredient])
+    } 
+
     return (
         <>
         <div className='info-Container'>
             <span className='info-text'>Escreva Os Ingredientes Disponiveis e Eu Te Darei Uma Receita !</span>
         </div>
 
-        <form onSubmit={SubmitIngredient}>
+        <form action={AddIngredient}>
             <div className='AddIngredient-Container'>
-            <input onSubmit={SubmitIngredient} className='AddIngredient-search' name='Ingredient' type="search" placeholder='Ex : Orégano , Queijo ...' />
-            <button onSubmit={SubmitIngredient} className='AddIngredient-button'> + Adicionar Ingrediente</button>
+            <input  className='AddIngredient-search' name='Ingredient' type="search" placeholder='Ex : Orégano , Queijo ...' />
+            <button className='AddIngredient-button'> + Adicionar Ingrediente</button>
             </div>
         </form>
 
-        <div className='Lista-Container'>
-            <span className='Lista-text'>Lista De Ingredientes :</span>
-        </div>
-        
-       {IngredientsList}
-        </>
+        {RenderList()}
+       </>
     )
 }
